d7376f019bdc27d9ab33f912f9a34d61e2399286
stevenh77 <stevenhollidge@hotmail.com>
2013-11-23 12:27:53 +0100

    initial commit

---
+++ RichTextBlockExample/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace RichTextBlockExample
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            string output = "Testing <bold>formatted</bold> text <underline>with</underline> a <italic>textblock</italic>";
+            TextBlockUsingBehaviour.Text = output;
+            TextBlockUsingAttachedProperty.SetValue(SupportRichText.RichTextProperty, output);
+        }
+    }
+}
---
+++ RichTextBlockExample/PropertyListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace RichTextBlockExample
+{
+    public class PropertyListener
+    {
+        /// <summary>
+        /// Listens for changes to dependency properties on a given FrameworkElement calls a callback on property change.
+        /// Usage example: PropertyListener.ListenForChange("IsBusy", BusyIndicator, (d,e) => DoStuff())
+        /// (may cause memory leaks)
+        /// </summary>
+        public void ListenForChange(string propertyName, FrameworkElement element, PropertyChangedCallback callback)
+        {
+            var b = new Binding(propertyName) { Source = element };
+            var prop = DependencyProperty.RegisterAttached(
+                "ListenAttached" + propertyName,
+                typeof(object),
+                typeof(Control),
+                new PropertyMetadata(callback));
+            element.SetBinding(prop, b);
+        }
+    }
+}
